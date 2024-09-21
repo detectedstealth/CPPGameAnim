6e93ef444c1d6131ab8e985afe453164bf8b6b46
Bruce Wade <bruce.wade@gmail.com>
2024-09-20 20:25:57 -0700

    added overloads for +,-,+ operators

--- math/vec3.h
+++ math/vec3.h
@@ -15,4 +15,12 @@ struct vec3 {
     inline vec3() : x(0.0f), y(0.0f), z(0.0f) {}
     inline vec3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
     inline vec3(float *fv) : x(fv[0]), y(fv[1]), z(fv[2]) {}
+
 };
+
+// Overloads for vector manipulation and calculations
+
+vec3 operator+(const vec3 &l, const vec3 &r);
+vec3 operator-(const vec3 &l, const vec3 &r);
+vec3 operator*(const vec3 &v, float f);
+vec3 operator*(const vec3 &l, const vec3 &r);
